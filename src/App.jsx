c2ee1925198f7aa6fd6c0a32b2a6323e329e1b4d
firsaavln <firsaavln@gmail.com>
2023-09-27 17:13:10 +0700

    baru sampai about

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+// components
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Services from "./components/Services";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <>
+      <div className="bg-site bg-no-repeat bg-cover overflow-hidden">
+        <Header />
+        <Banner />
+        <About />
+        <Services />
+        <Work />
+        <Contact />
+        <Nav />
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <div className="h-[4000px]"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
